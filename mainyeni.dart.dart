61772fb2d7d413621a23490eeb14ef714a5028be
Yiğit Gümüşmekikli <118180908+Yigtgumus@users.noreply.github.com>
2022-11-30 15:19:22 +0300

    Anasayfanın 2 öğesini içeren basit bir taslak

---
+++ mainyeni.dart.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            new IconButton(icon: new Icon(Icons.schedule_rounded), onPressed:_decrementCounter,color: Colors.red,iconSize: 50,),
+           Text(
+             'Çalışma Saatlerim',
+
+           ),
+            Text(
+                ''
+            ),
+            Text(
+                ''
+            ),
+            new IconButton(icon: new Icon(Icons.folder_open_rounded), onPressed: _decrementCounter,color:Colors.red,iconSize: 50,),
+            Text(
+              'Dökümanlarım',
+
+            ),
+        ]
+            )
+
+
+
+        ),
+
+      );
+
+
+
+
+
+  }
+}
