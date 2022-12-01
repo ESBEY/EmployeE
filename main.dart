8feabae1e32eab6d7c659566dd70cc6972c00ce6
Agustoo <55317471+Agustoo@users.noreply.github.com>
2022-12-01 04:02:43 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,15 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-import 'package:employe/login.dart';
-import 'package:employe/register.dart';
+import 'package:employe/menu.dart';
 import 'package:flutter/material.dart';
+import 'package:employe/login.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: MyLogin(),
     routes: {
-      'register': (context) => MyRegister(),
       'login': (context) => MyLogin(),
+      'menu':(context) => Menu(),
     },
   ));
 }
