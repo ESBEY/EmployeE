45e0a215b18dda909b3177c174388d044d9add4d
Yiğit Gümüşmekikli <118180908+Yigtgumus@users.noreply.github.com>
2022-12-07 23:36:21 +0300

    Update menu.dart

--- menu.dart
+++ menu.dart
@@ -1,137 +1,74 @@
-import 'package:denemeee/menu.dart';
+import 'package:denemeee/login.dart';
 import 'package:flutter/material.dart';
 
-
-class MyLogin extends StatefulWidget {
-  const MyLogin({Key? key}) : super(key: key);
+class Menu extends StatefulWidget {
+  const Menu({Key? key}) : super(key: key);
 
   @override
-  _MyLoginState createState() => _MyLoginState();
+  _MenuState createState() => _MenuState();
 }
 
-class _MyLoginState extends State<MyLogin> {
+class _MenuState extends State<Menu> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white10,
-      body:SafeArea(
-        child: Center(
-          child: SingleChildScrollView(
-
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return WillPopScope(child: Scaffold(
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(
-                  Icons.account_circle,
-                  color: Colors.red,
-                  size: 250,
-                ),
-                SizedBox(height: 25,),
-                Text(
-                  'Hoşgeldiniz!',
-                  style: TextStyle(
-                    fontSize: 65,
-                    color: Colors.red,fontFamily:"Times New Roman"
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Text(
-                  'Giriş yapabilirsiniz.',
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.red,
-                  ),
-                ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white10,
-                      border: Border.all(color: Colors.red),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        style: TextStyle(
-                          color: Colors.red,
-                        ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Email',
-                          hintStyle: TextStyle(
-                            color: Colors.red,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white10,
-                      border: Border.all(color: Colors.red),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        obscureText: true,
-                        style: TextStyle(
-                          color: Colors.red,
-                        ),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Şifre',
-                          hintStyle: TextStyle(
-                            color: Colors.red,
-                          ),
-
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: InkWell(
-                    child: Container(
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Giriş Yap',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ),
-                    ),
-                    onTap: (){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  Menu()));
-                    },
-                  ),
-                ),
-                SizedBox(height: 10,),
+                buildIconButton(Icons.doorbell_outlined, 'duyurular', () {}),
+                buildIconButton(Icons.schedule_rounded, 'Çalışma Saatlerim', () {}),
+                buildIconButton(Icons.folder_open_rounded, 'Dökümanlarım', () {}),
               ],
             ),
-          ),
-        ),
+            Text(''),
+            Text(''),
+            Text(''),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                buildIconButton(Icons.calendar_month_rounded, 'Planım', () {}),
+                buildIconButton(Icons.sunny, 'İzin Günlerim', () {}),
+                buildIconButton(Icons.attach_money_rounded, 'Maaşım', () {}),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => MyLogin()));
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.red,
+              ),
+              iconSize: 50,
+            ),
+            Text('Geri Dön',
+                style: TextStyle(
+                    color: Colors.white, fontFamily: 'Arial', fontSize: 15)),
+          ])),
+      backgroundColor: Colors.white10,
+    ), onWillPop: () async => false);
+  }
+
+  Widget buildIconButton(IconData icon, String title, void Function()? action,) {
+    return Column(children: [
+      IconButton(
+        icon: Icon(icon),
+        color: Colors.red,
+        iconSize: 50,
+        onPressed:action,
+
       ),
-    );
+      Text(
+        title,
+        style:
+            TextStyle(color: Colors.white, fontFamily: 'Arial', fontSize: 15),
+      ),
+    ]);
   }
 }
