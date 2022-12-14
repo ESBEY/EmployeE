44126b348b5df40d1718a4a2bd7522f0b65e9281
Yiğit Gümüşmekikli <yigtgumus@hotmail.com>
2022-12-14 23:45:40 +0300

    işçi-işveren menüsü ayarlandı
    .

--- lib/pages/home_employer.dart
+++ lib/pages/home_employer.dart
@@ -5,7 +5,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Employer extends StatefulWidget {
-
   const Employer({Key? key}) : super(key: key);
 
   @override
@@ -13,18 +12,48 @@ class Employer extends StatefulWidget {
 }
 
 class _EmployerState extends State<Employer> {
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
       body: Center(
-        child: Text(
-          'İşveren Sayfası',
-          style: TextStyle(fontSize: 50,color: Colors.red)
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            buildIconButton(Icons.speaker_notes, 'Duyuru Yap', () {},20),
+            buildIconButton(Icons.more_time, 'Saat Ayarla', () {},15),
+            buildIconButton(Icons.badge, 'İşçiler', () {},15),
+          ],
         ),
-      ),
+        SizedBox(
+          height: 50,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            buildIconButton(Icons.event, 'Plan', () {},20),
+            buildIconButton(Icons.light_mode, 'İzin Günleri', () {},20),
+
+          ],
+        ),
+      ])),
+      backgroundColor: Colors.black,
     );
   }
 
+  Widget buildIconButton(
+      IconData icon, String title, void Function()? action, double _fontSize) {
+    return Column(children: [
+      IconButton(
+        icon: Icon(icon),
+        color: Colors.red,
+        iconSize: 50,
+        onPressed: action,
+      ),
+      Text(
+        title,
+        style: TextStyle(
+            color: Colors.white, fontFamily: 'Arial', fontSize: _fontSize),
+      ),
+    ]);
+  }
 }
